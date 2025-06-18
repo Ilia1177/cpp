f6c4a8db3bc1088ac6687d1dde31bfade0759456
Nil Polack <npolack@2A2.42angouleme.fr>
2025-06-18 19:50:33 +0200

    main: add readme.txt

---
+++ README.txt
@@ -0,0 +1,1 @@
+Here is the CPP series of 42. Switch branch to switch module 0 - 9
--- ex00/newZombie.cpp
+++
@@ -1,6 +0,0 @@
-#include "Zombie.hpp"
-
-Zombie* newZombie(std::string name) {
-	Zombie *newBorn = new Zombie(name);
-	return (newBorn);
-}
