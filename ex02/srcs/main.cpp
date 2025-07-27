f655b242b408bf944be35d3f418b2b6449467c98
Ilia <npolack@student.42angouleme.fr>
2025-07-27 16:50:21 +0200

    CPP07: done 3/3

---
+++ ex02/srcs/main.cpp
@@ -0,0 +1,94 @@
+#include "Array.hpp"
+#include <exception>
+
+int main() {
+	int * a = new int();
+	std::cout << *a << std::endl;
+	
+	delete a;
+	{
+		Array<int> *arr = new Array<int>(5);
+
+		std::cout << "Int Array:" << std::endl;
+		try {
+			std::cout << *arr << std::endl;
+			(*arr)[0] = 48;
+			(*arr)[1] = 49;
+			(*arr)[2] = 50;
+			std::cout << *arr << std::endl;
+			(*arr)[20] = 20;
+		} catch (std::exception &e) {
+			std::cout << "exception caugth: " << e.what() << std::endl;
+		}
+		delete arr;
+	}
+	{
+		Array<char> *arr = new Array<char>(5);
+
+		std::cout << "Char Array:" << std::endl;
+		try {
+			std::cout << *arr << std::endl;
+			(*arr)[0] = 48;
+			(*arr)[1] = 49;
+			(*arr)[2] = 50;
+			std::cout << *arr << std::endl;
+			(*arr)[20] = 20;
+		} catch (std::exception &e) {
+			std::cout << "exception caugth: " << e.what() << std::endl;
+		}
+		delete arr;
+	}
+
+	{
+		Array<double> *arr = new Array<double>(5);
+
+		std::cout << "Double Array:" << std::endl;
+		try {
+			std::cout << *arr << std::endl;
+			(*arr)[0] = 48.98;
+			(*arr)[1] = 49.54;
+			(*arr)[2] = 50.2;
+			std::cout << *arr << std::endl;
+			(*arr)[20] = 20;
+		} catch (std::exception &e) {
+			std::cout << "exception caugth: " << e.what() << std::endl;
+		}
+		delete arr;
+	}
+
+	{
+		Array<std::string> *arr = new Array<std::string>(5);
+
+		std::cout << "Std::string Array:" << std::endl;
+		try {
+			std::cout << *arr << std::endl;
+			(*arr)[0] = "48 citron";
+			(*arr)[1] = "49";
+			(*arr)[2] = "orteils";
+			std::cout << *arr << std::endl;
+			(*arr)[20] = "20";
+		} catch (std::exception &e) {
+			std::cout << "exception caugth: " << e.what() << std::endl;
+		}
+		delete arr;
+	}
+
+	{
+		Array<std::pair<int, std::string> > *arr = new Array<std::pair<int, std::string> >(8);
+
+		std::cout << "Std::pair Array:" << std::endl;
+		try {
+			std::cout << *arr << std::endl;
+			(*arr)[0] = std::make_pair(48, "citron");
+			(*arr)[1] = std::make_pair(48, "citron");
+			(*arr)[2] = std::make_pair(48, "citron");
+			(*arr)[3] = std::make_pair(48, "citron");
+			std::cout << *arr << std::endl;
+		} catch (std::exception &e) {
+			std::cout << "exception caugth: " << e.what() << std::endl;
+		}
+		delete arr;
+	}
+	return 0;
+}
+
