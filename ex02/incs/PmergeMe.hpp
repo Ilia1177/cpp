2db3751f323780ab058a5a37f348a4a792bbea8d
Ilia <npolack@student.42angouleme.fr>
2025-08-22 17:47:48 +0200

    CPP09: ex02: use container<elem_t>

--- ex02/incs/PmergeMe.hpp
+++ ex02/incs/PmergeMe.hpp
@@ -6,10 +6,31 @@
 # include <cstdlib>
 # include <stdexcept>
 # include <exception>
-#include <sstream>
+# include <sstream>
 # include <cmath>
 
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+
+
 int toInt(const std::string& str, char** endptr);
+
+typedef struct elem_s {
+	elem_s() {};
+	elem_s(int val, std::string lab): value(val), label(lab) {};
+	elem_s(int val): value(val), label("") {};
+	elem_s(const elem_s& other): value(other.value), label(other.label) {};
+	elem_s& operator=(const elem_s &other) {this->value = other.value; this->label = other.label; return *this;}
+	bool operator<(elem_s &other) {return this->value < other.value;}
+	int value;
+	std::string label;
+} elem_t;
 
 template<typename C>
 class PmergeMe
@@ -26,7 +47,7 @@ class PmergeMe
 
 			//iss.init(input);
 			while (iss >> num) {
-				_arr.push_back(num);
+				_arr.push_back(elem_t(num));
 			}
 
 			//std::string str = input;
@@ -38,12 +59,19 @@ class PmergeMe
 			//}	
 		}
 
-		void print(C c)
+		void print(C &c)
 		{
 			typename C::iterator it;
 			typename C::iterator ite = c.end() - 1;
+
 			for (it = c.begin(); it != c.end(); ++it) {
-				std::cout << *it;
+				if (it->label == "") {
+					std::cout << it->value;
+				} else if (it->label == "a") {
+					std::cout << GREEN << it->value << RESET;
+				} else if (it->label == "b") {
+					std::cout << RED << it->value << RESET;
+				}
 				if (it != ite)
 					std::cout << ", ";
 			}
@@ -74,9 +102,9 @@ class PmergeMe
 				std::cout << "mid: " << mid << " - low: " << low << " - high: " << high << std::endl;
 				typename C::iterator it = arr.begin() + mid;
 
-				if (*it == x) {
+				if (it->value == x) {
 					return it; // exact match
-				} else if (*it > x) {
+				} else if (it->value > x) {
 					best = it;          // candidate: closest so far <= x
 					high = mid - size;  // search left
 				} else {
@@ -130,8 +158,8 @@ class PmergeMe
 //			return -1;
 //		}
 
-		void swap(int &a, int &b) {
-			int tmp = a;
+		void swap(elem_t &a, elem_t &b) {
+			elem_t tmp = a;
 			a = b;
 			b = tmp;
 		}
@@ -147,6 +175,27 @@ class PmergeMe
 		    return round((std::pow(2, k + 1) + std::pow(-1, k)) / 3);
 		}
 
+		void init_label(void) {
+			typename C::iterator it = _arr.begin();
+			for (; it < _arr.end(); ++it) {
+					it->label = "";
+				}
+			}
+
+		void assign_label(const size_t nb_pairs, const size_t elem_size)
+		{
+			typename C::iterator it = _arr.begin();
+			for (; it < _arr.begin() + elem_size * (nb_pairs * 2); it += elem_size * 2) {
+				for (size_t i = 0; i < elem_size; ++i) {
+					(it + i)->label = "a";
+				}
+			}
+			for (it = _arr.begin() + elem_size; it < _arr.begin() + elem_size * (nb_pairs * 2); it += elem_size * 2) {
+				for (size_t i = 0; i < elem_size; ++i) {
+					(it + i)->label = "b";
+				}
+			}
+		}
 		// sort elements inside of pairs by comparing them against each others: a <? b
 		// element are describe by their last number inside it
 		// for elem_size = 1 -> pair_size = 2;
@@ -175,25 +224,30 @@ class PmergeMe
 		void insert_in(C& arr, typename C::iterator it, size_t elem_size) {
 			typename C::iterator idx;
 			std::cout << "insert in: " << std::endl;
-			idx = binarySearch(arr, *it, elem_size);
-			std::cout << "idx to insert from is: " << *idx << std::endl;
-			arr.insert(idx - (elem_size - 1), it - (elem_size - 1), it + 1);
+			idx = binarySearch(arr, it->value, elem_size);
+			std::cout << "idx to insert from is: " << idx->value << std::endl;
+			if (it->label == "")
+				arr.insert(idx - (elem_size - 1), *it);
+			else
+				arr.insert(idx - (elem_size - 1), it - (elem_size - 1), it + 1);
 		}
 
 		void merge_insertion_sort(size_t elem_size)
 		{
-
-			size_t nb_elems = _arr.size() / elem_size;
+			size_t nb_pairs = _arr.size() / elem_size / 2;
+			size_t nb_elems = nb_pairs * 2;
 			//if (elem_size > _arr.size() / 2)
-			if (nb_elems < 2)
+			if (nb_pairs < 1)
 				return ;
 
-			std::cout << "element size: " << elem_size << " - nb of elements: " << nb_elems << std::endl;
+			std::cout << "element size: " << elem_size << " - nb of elements: " << nb_elems << " - nb of pairs: " << nb_pairs << std::endl;
 
+			init_label();
 			std::cout << "Before: "; print(_arr);
 			sort_pairs(elem_size);
+			assign_label(nb_pairs, elem_size);
 			std::cout << "Result: "; print(_arr);
-			//merge_insertion_sort(elem_size * 2);
+			merge_insertion_sort(elem_size * 2);
 			
 			std::cout << "Part 2" << std::endl << std::endl;
 
@@ -226,23 +280,26 @@ class PmergeMe
 				}
 			}
 			// push the remainder items (not paired items) in pend;
-			//for (it = _arr.begin() + elem_size * nb_elems + 1; it < _arr.end(); ++it) {
-			//	pend.push_back(*it);
-			//}
+			for (it = _arr.begin() + elem_size * nb_elems; it < _arr.end(); ++it) {
+				pend.push_back(*it);
+			}
 
 			std::cout << "size :" << elem_size << std::endl;
 			std::cout << "main list: "; print(main);
 			std::cout << "pend list: "; print(pend);
 
 			//std::cout << *it << std::endl;
-			//while (!pend.empty()) {
-			//	it = pend.end() - 1;
-			//	std::cout << "pend size: " << pend.size() << " - it to insert: " << *it <<std::endl;;
-			//	insert_in(main, it, elem_size);
-			//	pend.erase(it - (elem_size - 1), it + 1);
-			//	std::cout << "main list is: "; print(main);
-			//}
-			//std::cout << "reuslt main list: "; print(main);
+			while (!pend.empty()) {
+				it = pend.begin();
+				std::cout << "pend size: " << pend.size() << " - it to insert: " << it->value <<std::endl;;
+				insert_in(main, it, elem_size);
+				if (it->label == "")
+					pend.erase(it);
+				else
+					pend.erase(it, it + elem_size);
+				std::cout << "main list is: "; print(main);
+			}
+			std::cout << "reuslt main list: "; print(main);
 		}
 
 	private:
--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -4,13 +4,15 @@
 int main(int ac, char **av) 
 {
 	std::string query;
-	PmergeMe<std::deque<int> > 	deq;
-	PmergeMe<std::vector<int> > vec;
+	PmergeMe<std::deque<elem_t> > 	deq;
+	PmergeMe<std::vector<elem_t> > vec;
 
-	if (ac != 2)
-		return 1;
+	(void)av;
+	(void)ac;
 
-	deq.read(av[1]);
+	std::string set1 = "11 2 17 0 16 8 6 15 10 3 21 1 18 9 14 19 12 5 4 20 13 7";
+	std::string set2 = "30 49 60 1 31 29 42 46 13 8 5";
+	deq.read(set2);
 	//deq.init_pair();
-	deq.merge_insertion_sort(4);
+	deq.merge_insertion_sort(1);
 }
