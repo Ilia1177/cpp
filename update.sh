303e831cde7ef2dc14149e8d5a26ab5c8b5da8df
Nil Polack <npolack@1G6.42angouleme.fr>
2025-09-11 18:28:50 +0200

    Modify update.sh to take the remote in argument

--- update.sh
+++ update.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Check if remote is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <remote>"
+    exit 1
+fi
+
+REMOTE=$1
+
 # Function to add or update subtree
 manage_subtree() {
     local folder=$1
@@ -7,15 +15,16 @@ manage_subtree() {
     
     if [ -d "$folder" ]; then
         echo "Updating existing subtree: $folder"
-        git subtree pull --prefix=$folder home $branch --squash
+        git subtree pull --prefix=$folder $REMOTE $branch --squash
     else
         echo "Adding new subtree: $folder"
-        git subtree add --prefix=$folder home $branch --squash
+        git subtree add --prefix=$folder $REMOTE $branch --squash
     fi
 }
 
 # Ensure we're on main branch
 git checkout main
+
 # Process CPP00 through CPP09
 for i in {0..9}; do
     branch=$(printf "CPP%02d" $i)
