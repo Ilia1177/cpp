c6005d967d5a63b776550cea79a1a1e26166b40d
ilia <npolack@student.42angouleme.fr>
2025-09-22 11:05:24 +0200

    fix: use std::vector in ex01

--- ex01/srcs/Span.cpp
+++ ex01/srcs/Span.cpp
@@ -1,126 +1,122 @@
 #include "Span.hpp"
-#include <stdexcept>
 
 // Destructor
-Span::~Span(void) {delete [] _tab;}
+Span::~Span(void) {}
 
 // Default constructor
-Span::Span(size_t n): _size(n), _index(0) {_tab = new int[n];}
+Span::Span(size_t n): _index(0), _max(n), _vec(n) {}
 
 // Copy constructor
-Span::Span(const Span &other): 
-	_size(other._size), _index(other._index), _tab(new int[other._size]) {
-	for (size_t i = 0; i < other._size; ++i) {
-		this->_tab[i] = other._tab[i];
-	}
-}
+Span::Span(const Span &other): _index(other._index), _max(other._max), _vec(other._vec) {}
 
 // Assignment operator overload
 Span &Span::operator=(const Span &other) {
 	if (this != &other) {
-		if (this->_tab) delete [] _tab;
 		this->_index = other._index;
-		this->_size = other._size;
-		this->_tab = new int[this->_size];
-		for (size_t i = 0; i < this->_size; ++i) {
-			this->_tab[i] = other._tab[i];
-		}
+		this->_max = other._max;
+		this->_vec = other._vec;
 	}
     return (*this);
 }
 
 const int& Span::operator[](const size_t index) const {
-	if (index >= _size)
-		throw std::out_of_range("out of range");
-	return _tab[index];
+	if (index >= _index)
+		throw std::invalid_argument("index not initialized");
+	else if (index >= _max)
+		throw std::out_of_range("index out of range");
+	return _vec[index];
 }
 
 int& Span::operator[](const size_t index) {
-	if (index >= _size)
-		throw std::out_of_range("out of range");
-	return _tab[index];
+	if (index >= _index)
+		throw std::invalid_argument("index not initialized");
+	else if (index >= _max)
+		throw std::out_of_range("index out of range");
+	return _vec[index];
 }
 
-size_t Span::size(void) const {return _size;}
+size_t Span::size(void) const {return _index;}
 
-size_t Span::index(void) const {return _index;}
+size_t Span::max(void) const {return _max;}
 
 void Span::addNumber(int num) {
-	if (_index >= _size)
-		throw std::out_of_range("Span is full");
-	this->_tab[_index++] = num;
+	if (_index >= _max)
+		throw std::out_of_range("Span is at max");
+	_vec[_index++] = num;
 }
 
 size_t Span::shortestSpan() {
-	if (_size <= 1 || _index <= 1)
-		throw std::out_of_range("No span can be found");
+    if (_vec.size() < 2) {
+        throw std::out_of_range("No span can be found");
+    }
+	std::vector<int>::iterator min_it;
+    std::vector<int> tmp = _vec;
+	
+    std::sort(tmp.begin(), tmp.end());
+    std::vector<int> diffs(tmp.size());
+    std::adjacent_difference(tmp.begin(), tmp.end(), diffs.begin());
+	min_it = std::min_element(diffs.begin() + 1, diffs.end()); 
 
-	size_t	currentSpan;
-	size_t	shortest = ABS(_tab[0] - _tab[1]);
-
-	for (size_t i = 0; i < _index; ++i) {
-		for (size_t j = i + 1; j < _index; ++j) {
-			currentSpan = ABS(_tab[i] - _tab[j]);
-			if (currentSpan < shortest)
-				shortest = currentSpan;
-		}
-	}
-	return (shortest);
+    return static_cast<size_t>(*min_it);
 }
 
 size_t Span::longestSpan(void) {
-	if (_size <= 1 || _index <= 1)
-		throw std::out_of_range("No span can be found");
-
-	int	currentSpan;
-	int longest = ABS(_tab[0] - _tab[1]);
-
-	for (size_t i = 0; i < _index; ++i) {
-		for (size_t j = i + 1; j < _index; ++j) {
-			currentSpan = ABS(_tab[i] - _tab[j]);
-			if (currentSpan > longest)
-				longest = currentSpan;
-		}
+	if (_vec.size() < 2) {
+        throw std::out_of_range("No span can be found");
 	}
-	return (longest);
+	std::vector<int> tmp = _vec;
+
+	std::sort(tmp.begin(), tmp.end());
+	size_t diff = *(tmp.end() - 1) - *tmp.begin();
+
+	return diff;
 }
 
-const int* Span::begin(void) const {
-	return _tab;
+std::vector<int>::iterator Span::begin(void) {
+	return _vec.begin();
 }
 
-const int* Span::end(void) const {
-	return _tab + _index;
+std::vector<int>::iterator Span::end(void) {
+	if (_index == 0) return _vec.end();
+	return _vec.begin() + _index;
 }
 
-void Span::assign(size_t n, int value) {
-	if (_tab) delete [] _tab;
-	_size = n;
+void Span::assign(size_t n, int value)
+{
+	if (n > _max) {
+		_vec.assign(_max, value);
+		_index = _max;
+		throw std::out_of_range("reach maximum");
+	}
+	_vec.assign(n, value);
 	_index = n;
-	_tab = new int[n];
-	for (size_t i = 0; i < n; ++i) {
-		_tab[i] = value;
-	}
 }
 
 //Be carreful to give pointer from the same object or it will have an UNDEFINED BEHAVIOR
-void Span::assign(const int* begin, const int* end) {
+void Span::assign(std::vector<int>::iterator begin, std::vector<int>::iterator end) {
 	if (begin > end)
-		throw std::invalid_argument("begin > end");
-	if (_tab) delete [] _tab;
-	_size = end - begin;
-	_index = _size;
-	_tab = new int[_size];
-	size_t i = 0;
-	for (const int *it = begin; it < end && i < _size; ++it) {
-		_tab[i++] = *it;
+		throw std::invalid_argument("start is beyond end");
+	if (static_cast<size_t>(end - begin) > _max) {
+		_vec.assign(begin, begin + _max);
+		_index = _max;
+		throw std::out_of_range("out of range");
 	}
+	_vec.assign(begin, end);
+	_index = static_cast<size_t>(end - begin);
+}
+
+void	Span::rfill(int min, int max) {
+	for (size_t i = 0; i < _max; ++i) {
+		_vec[i] = min + std::rand() % (max - min + 1);
+	}
+	_index = _max;
 }
 
 std::ostream &operator<<(std::ostream& os, const Span& s) {
-	for (size_t i = 0; i < s.index(); ++i) {
+	std::cout << "size: " << s.size() << " max: " << s.max() << "\n";
+	for (size_t i = 0; i < s.size(); ++i) {
 		os << s[i];
-		if (i != s.index() - 1) os << ", ";
+		if (i != s.size() - 1) os << ", ";
 	}
 	return os;
 }
--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -1,12 +1,5 @@
 #include "Span.hpp"
-#include <ctime>
-#include <cstdlib>
 
-void	fillSpan(Span& s, int min, int max) {
-	for (size_t i = s.index(); i < s.size(); ++i) {
-		s.addNumber(min + std::rand() % (max - min + 1));
-	}
-}
 
 
 int main() {
@@ -15,24 +8,44 @@ int main() {
 	Span nums(10);
 	Span bucket(20);
 	try {
-		std::cout << "filling nums..." << std::endl;
-		fillSpan(nums, -534999, 456888);
+		std::cout << "filling nums span with random int" << std::endl;
+		nums.rfill(-534999, 456888);
 		std::cout << "nums: " << nums << std::endl;
-		std::cout << "assign nums from int: " << std::endl;
+		std::cout << "assign nums with multiple int in one row: " << std::endl;
 		nums.assign(5, 3);
 		std::cout << "nums: " << nums << std::endl;
-		std::cout << "filling bucket..." << std::endl;
-		fillSpan(bucket, -28, 199);
+		std::cout << "filling bucket span with random int" << std::endl;
+		bucket.rfill(-28, 199);
 		std::cout << "bucket: " << bucket << std::endl;
 		std::cout << "assign nums from bucket pointers: " << std::endl;
 		nums.assign(bucket.begin(), bucket.end());
 		std::cout << "nums: " << nums << std::endl;
 		std::cout << "Shortest span is: " << nums.shortestSpan() << std::endl;
 		std::cout << "Longest span is: " << nums.longestSpan() << std::endl;
+		nums.addNumber(77); // should throw
 	} catch (std::exception &e) {
 		std::cout << "exception caught: " << e.what() << std::endl;
 	}
+	try {
+		nums.assign(10, 99); 
+		std::cout << nums << std::endl;
+	} catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	try {
+		nums.assign(11, 99); 
+		std::cout << nums << std::endl;
+	} catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	try {
+		nums.assign(3, 33); 
+		std::cout << nums << std::endl;
+	} catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
 	{
+		std::cout << "Subject test: " << std::endl;
 		Span sp = Span(5);
 		sp.addNumber(6);
 		sp.addNumber(3);
