67ea75a098e795745b2316a8ced0fe6701ab8f0d
Nil Polack <npolack@3A5.42angouleme.fr>
2025-10-29 22:43:18 +0100

    [fix] works with multiple args

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -2,11 +2,19 @@
 
 int main(int ac, char **av) 
 {
+	std::string input;
 	if (ac < 2) {
 		std::cerr << "Need one argument\n";
 		return 1;
+	} else if (ac > 2) {
+		for (int i = 1; i < ac; i++) {
+			input += std::string(av[i]) + " ";
+		}
+	} else {
+		input = std::string(av[1]);
 	}
-	RPN polish(av[1]);
+	std::cout << "Input: "<< input << std::endl;
+	RPN polish(input);
 
 	try {
 		std::cout << "result: " << polish.exec() << std::endl;
